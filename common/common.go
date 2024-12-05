40da82302dfccc4acd6adc5881820cd3821def87
jdotrjs <jdotrjs@noreply.users.github.com>
2024-12-05 14:08:40 -0800

    promote common upgrade

--- common/common.go
+++ common/common.go
@@ -9,7 +9,23 @@ import (
 	"strings"
 )
 
-func processInput[T any](processFunc func(string) T) chan T {
+type LineProcessor[T any] interface {
+	Do(s string) T
+}
+
+type simpleProcessor[T any] struct {
+	processFunc func(string) T
+}
+
+func (sp simpleProcessor[T]) Do(s string) T {
+	return sp.processFunc(s)
+}
+
+func processInputSimple[T any](processFunc func(string) T) chan T {
+	return processInput(simpleProcessor[T]{processFunc}, false)
+}
+
+func processInput[T any](processor LineProcessor[T], pass_newlines bool) chan T {
 	output_chan := make(chan T)
 
 	go func() {
@@ -24,15 +40,15 @@ func processInput[T any](processFunc func(string) T) chan T {
 				break
 			}
 
-			if str == "" {
+			if str == "" && !pass_newlines {
 				continue
 			}
 
-			output_chan <- processFunc(str)
+			output_chan <- processor.Do(str)
 		}
 
 		if str != "" {
-			last_entry := processFunc(str)
+			last_entry := processor.Do(str)
 			output_chan <- last_entry
 		}
 		close(output_chan)
@@ -46,6 +62,13 @@ func Must[T any](t T, e error) T {
 		log.Panic(e)
 	}
 
+	return t
+}
+
+func MustOK[T any](t T, ok bool) T {
+	if !ok {
+		log.Panic("Expected ok but got not ok")
+	}
 	return t
 }
 
